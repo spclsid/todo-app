fc63975cf0be24e54d32bbe303ee38725ff28358
Siddharth2003 <sidpranshu999@gmail.com>
2024-10-11 14:08:59 +0530

    checkbox done

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Navbar } from './components/Navbar'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
@@ -16,7 +17,7 @@ function App() {
   }
 
   const handleAdd = ()=>{
-    setTodos([...todos, {todo, isCompleted: false}])
+    setTodos([...todos, {id:uuidv4(),todo, isCompleted: false}])
     setTodo("")
   }
   
@@ -24,7 +25,20 @@ function App() {
   const handleChange = (e)=>{
     setTodo(e.target.value)
   }
+  
+  const handleCheckbox = (e)=>{
+    let id= e.target.name;
+    let index=todos.findIndex(item=>{
+      return item.id == id;
+    })
+    let newTodos=todos;
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos)
+  }
+  
+//}
 
+  
   return (
     <>
     <Navbar/>
@@ -40,7 +54,8 @@ function App() {
 
             
             return <div key={todo} className='todo flex w-1/5 my-3 justify-between'>
-              <div className={item.isCompleted?"":"line-through"}>{item.todo}</div>
+              <input name={todo.id} onChange={handleCheckbox} type="checkbox" value={todo.isCompleted} id=""/>
+              <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
               <div className="buttons">
                 <button onClick={handleEdit} className='bg-violet-800 hover:bg-violet-950 p-3 py-1 text-sm font-bold text-white rounded-md mx-1'>Edit</button>
                 <button onClick={handleDelete}  className='bg-red-600 hover:bg-red-800 p-3 py-1 text-sm font-bold text-white rounded-md mx-1'>Delete</button>
