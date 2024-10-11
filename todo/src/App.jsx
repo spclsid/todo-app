bec1b0acee22cf238581946fd68313dab1457c1c
Siddharth2003 <sidpranshu999@gmail.com>
2024-10-11 15:06:34 +0530

    edit button done

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -8,7 +8,14 @@ function App() {
   const [todos, setTodos] = useState([])
 
 
-  const handleEdit = ()=>{
+  const handleEdit = (e,id)=>{
+    let t=todos.filter (i=>i.id=== id)
+    setTodo(t[0].todo)
+
+    let newTodos = todos.filter(item=>{
+      return item.id!== id
+    });
+    setTodos(newTodos)
     
 
   }
@@ -41,10 +48,6 @@ function App() {
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
     setTodos(newTodos)
   }
-  
-//}
-
-  
   return (
     <>
     <Navbar/>
@@ -52,7 +55,7 @@ function App() {
         <div className='addTodo my-5'>
           <h2 className='text-lg font-bold'>Add a Todo </h2>
           <input onChange={handleChange} value={todo} type='text' className='w-1/3'/>
-          <button onClick={handleAdd} className='bg-green-500 hover:bg-green-700 p-3 py-1 font-bold text-zinc-900 rounded-md mx-6'>Add</button>
+          <button onClick={handleAdd} className='bg-green-500 hover:bg-green-700 p-3 py-1 font-bold text-zinc-900 rounded-md mx-6'>Save</button>
           </div>
           <h2 className='text-2xl font-bold'>Your Todos</h2>
           <div className='todos'>
@@ -66,7 +69,7 @@ function App() {
               <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
               </div>
               <div className="buttons">
-                <button onClick={handleEdit} className='bg-violet-800 hover:bg-violet-950 p-3 py-1 text-sm font-bold text-white rounded-md mx-1'>Edit</button>
+                <button onClick={(e)=>handleEdit(e,item.id)} className='bg-violet-800 hover:bg-violet-950 p-3 py-1 text-sm font-bold text-white rounded-md mx-1'>Edit</button>
                 <button onClick={(e)=>handleDelete(e,item.id)}  className='bg-red-600 hover:bg-red-800 p-3 py-1 text-sm font-bold text-white rounded-md mx-1'>Delete</button>
               </div>
             </div>
